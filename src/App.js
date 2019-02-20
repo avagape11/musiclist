485608afcb0c7f55698ed1f0aaa0a576f4390eb1
aga <47752783+avagape11@users.noreply.github.com>
2019-02-20 20:51:34 +0200

    add-material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import { TextField, Button } from '@material-ui/core';
+
 class App extends Component {
+  state = {
+    searchTerm: ''
+  }
+  onTextChange = (event) => {
+    const value = event.target.value;
+    this.setState({searchTerm: value});
+  }
+
+  onSearchClick = () => {
+    console.log(this.state.searchTerm);
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Photos
+          </Typography>
+          <TextField  
+            placeholder="Search" 
+            onChange={this.onTextChange} 
+            value={this.state.searchTerm}
+            />
+          <Button onClick={this.onSearchClick} disabled={this.state.searchTerm.length === 0}>Go</Button>
+        </Toolbar>
+        </AppBar>
         </header>
+      
       </div>
     );
   }
